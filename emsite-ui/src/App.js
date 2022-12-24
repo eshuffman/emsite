a5b43d5fa33687cdcec095904ee29aec0bf856cc
Emily Huffman <ehuffman@catalyte.io>
2022-12-24 14:33:21 -0600

    forgot to create new branch

--- emsite-ui/src/App.js
+++ emsite-ui/src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Emily Huffman</h1>
+      <h2>Software Engineer | Writer | Child-Wrangler</h2>
     </div>
   );
 }
