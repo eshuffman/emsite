825898741ce846c3c6f826c31105ae55b79a8b9b
Emily Huffman <ehuffman@catalyte.io>
2022-12-25 14:06:08 -0600

    styling change linked to button

--- emsite-ui/src/App.js
+++ emsite-ui/src/App.js
@@ -1,20 +1,20 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  const [setStylingOn, stylingOn] = useState(false)
+  const [stylingOn, setStylingOn] = useState(false)
 
-  function handleClick() {
-    setStylingOn(!stylingOn)
-  }
+  
   return (
-    <div className={stylingOn === true ? "body" : null}>
-      <h1>Emily Huffman</h1>
-      <h2>Software Engineer | Writer | Child-Wrangler</h2>
-      <p>Hi! I'm Emily. Welcome to my little spot on the web! You may notice it looks pretty boring around here. If you want to fancy it up, feel free to push the button below.</p>
-      <button onClick={handleClick}>Push me!</button>
+    <div>
+      <div className={stylingOn === true ? "body" : null}>
+        <h1>Emily Huffman</h1>
+        <h2>Software Engineer | Writer | Child-Wrangler</h2>
+        <p>Hi! I'm Emily. Welcome to my little spot on the web! You may notice it looks pretty boring around here. If you want to fancy it up, feel free to push the button below.</p>
+        <p>Styling is {stylingOn}</p>
+      </div>
+      <button onClick={(() => setStylingOn(!stylingOn))}>Push me!</button>
     </div>
   );
 }
